3e143446b14f8c65962102ea23cd068ac25f8262
seitez-netizen <seitez@michelle-e.world>
2026-01-03 03:19:59 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// CSSのimport行は削除済み
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
